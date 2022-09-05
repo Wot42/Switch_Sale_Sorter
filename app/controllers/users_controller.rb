8c14ba85fdbeb5cdd09558f187833bebf0264837
Yemi Oniwinde <yoniwinde@hotmail.com>
2022-09-05 17:10:24 +0100

    controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,22 @@ class UsersController < ApplicationController
   end
 
   def baned
+    @user = User.find(params[:id])
+    @baned = User.games
   end
+
+  def shop
+    @user = User.find(params[:id])
+    @games = @user.games
+  end
+
+  def basket
+    @user = User.find(params[:id])
+    @games = []
+    @user.games.each do |game|
+      @games << shop.game
+    end
+  end
+
+
 end
---
+++ test/controllers/ban_hammers_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class BanHammersControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
