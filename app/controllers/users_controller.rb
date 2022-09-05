00672aa892dc45d7c9318068cd40c2a2f428b3e5
Yemi Oniwinde <yoniwinde@hotmail.com>
2022-09-05 17:13:54 +0100

    controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,12 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @show = User.games
   end
 
   def baned
     @user = User.find(params[:id])
     @baned = User.games
   end
-
-  def shop
-    @user = User.find(params[:id])
-    @games = @user.games
-  end
-
-  def basket
-    @user = User.find(params[:id])
-    @games = []
-    @user.games.each do |game|
-      @games << shop.game
-    end
-  end
-
 
 end
