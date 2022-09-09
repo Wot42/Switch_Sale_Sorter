7efeac711460bce2b627d5dd8fc8295a5719d1ba
Yemi Oniwinde <yoniwinde@hotmail.com>
2022-09-09 14:19:29 +0100

    filter completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,48 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @games = Game.all
     if params.present?
       if params[:filter] == "action"
-        @games = Game.filtering(params[:filter])
+        games_all = Game.filtering(params[:filter])
       elsif params[:filter] == "adventure"
-        # params[:filter] == "Fighting"
-        # params[:filter] == "Health & Fitness"
-        # params[:filter] == "Music"
-        # params[:filter] == "Party"
-        # params[:filter] == "Platformer"
-        # params[:filter] == "Puzzle"
-        # params[:filter] == "Racing"
-        # params[:filter] == "RPG"
-        # params[:filter] == "Shooter"
-        # params[:filter] == "Simulation"
-        # params[:filter] == "Sports"
-        # params[:filter] == "Strategy"
+        games_all = Game.filtering(params[:filter])
+      elsif params[:filter] == "fighting"
+        games_all = Game.filtering(params[:filter])
+      elsif params[:filter] == "Health & Fitness"
+        games_all = Game.filtering(params[:filter])
+      elsif params[:filter] == "music"
+        games_all = Game.filtering(params[:filter])
+      elsif params[:filter] == "party"
+        games_all = Game.filtering(params[:filter])
+      elsif params[:filter] == "platformer"
+        games_all = Game.filtering(params[:filter])
+      elsif params[:filter] == "puzzle"
+        games_all = Game.filtering(params[:filter])
+      elsif params[:filter] == "racing"
+        games_all = Game.filtering(params[:filter])
+      elsif params[:filter] == "rpg"
+        games_all = Game.filtering(params[:filter])
+      elsif params[:filter] == "shooter"
+        games_all = Game.filtering(params[:filter])
+      elsif params[:filter] == "simulation"
+        games_all = Game.filtering(params[:filter])
+      elsif params[:filter] == "sports"
+        games_all = Game.filtering(params[:filter])
+      elsif params[:filter] == "strategy"
+        games_all = Game.filtering(params[:filter])
+      elsif params[:sort] == "lowest"
+        games_all = Game.all.order("price, ASC")
+      elsif params[:sort] == "Title(A-Z)"
+        games_all = Game.all.order("title, ASC")
+      else
+        games_all = Game.all
       end
-
     else
-      @games = Game.all
+      games_all = Game.all
 
     end
 
+    @games = games_all
     @is_uers = @user == current_user
     @filter = params[:filter]
     @sort_by = params[:sortby]
