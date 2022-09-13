57fa293694f293adb0da64e37f77f7e2361f5f43
Yemi Oniwinde <yoniwinde@hotmail.com>
2022-09-13 12:29:23 +0100

    newindexpage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,10 +5,62 @@ class PagesController < ApplicationController
     @games = Game.all
     # sort
 
+    games_all = Game.all
+    if params.present?
+
+      if params[:filter]
+        games_all = Game.filtering(params[:filter])
+        if params[:sort]
+          if params[:sort] == "highest"
+            games_all = Game.order("price DESC").and(Game.where("'#{params[:filter]}' = ANY (tags)"))
+          elsif params[:sort] == "title ASC"
+            games_all = Game.order("games.title ASC").and(Game.where("'#{params[:filter]}' = ANY (tags)"))
+          elsif params[:sort] == "title DESC"
+            games_all = Game.order("games.title DESC").and(Game.where("'#{params[:filter]}' = ANY (tags)"))
+          elsif params[:sort] == "lowest"
+            games_all = Game.order("price ASC").and(Game.where("'#{params[:filter]}' = ANY (tags)"))
+          elsif params[:sort] == "biggestdiscount"
+            games_all = Game.order("discount_percentage DESC").and(Game.where("'#{params[:filter]}' = ANY (tags)"))
+          elsif params[:sort] == "sale_start"
+            games_all = Game.order("sale_start DESC").and(Game.where("'#{params[:filter]}' = ANY (tags)"))
+          end
+        end
+      else
+        games_all = Game.all
+      end
+    else
+      games_all = Game.all
+    end
+
+    @games = []
+    games_all.each do |game|
+      show = true
+      hammers = game.BanHammers
+      hammers.each do |hammer|
+        show = false if hammer.user == @user
+      end
+      @games.push(game) if show
+    end
+
+
     # filter
     @users = User.all
     @user = current_user
-    @filter = params[:filter]
-    @sort_by = params[:sortby]
+    # @filter = params[:filter]
+    # @sort_by = params[:sortby]
+  end
+
+
+  def banned
+    @user = current_user
+
+    hammers = @user.BanHammers
+    @games = []
+    hammers.each do |hammer|
+      @games.push(hammer.game)
+    end
+
+    # yemis code here but modified
+
   end
 end
