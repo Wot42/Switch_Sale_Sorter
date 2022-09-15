2a0bf5ae0a18d18f60830888ebcbeb80660cac23
rafaelpando <rafaelpando9@gmail.com>
2022-09-15 15:22:35 +0100

    name and picture in form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:left_color, :right_color])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:left_color, :right_color, :user_name, :picture])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:left_color, :right_color])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:left_color, :right_color, :user_name, :picture])
   end
 end
